229326caad64d73b40c073fbf1027a4e2f80d227
kosenda <kkkk0630@ezweb.ne.jp>
2024-02-18 16:09:22 +0900

    Initial commit

---
+++ app/src/main/java/ksnd/devicecredentialsample/MainActivity.kt
@@ -0,0 +1,43 @@
+package ksnd.devicecredentialsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ksnd.devicecredentialsample.ui.theme.DeviceCredentialSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DeviceCredentialSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DeviceCredentialSampleTheme {
+        Greeting("Android")
+    }
+}
